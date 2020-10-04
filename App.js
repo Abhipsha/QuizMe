790c86d1bf0e6f2679853bcf40b4ae296b3fd63b
Abhipsha Rawat <abhipsha@Abhipshas-MacBook-Pro.local>
2020-10-05 00:32:05 +0530

    minor fix

--- App.js
+++ App.js
@@ -4,9 +4,7 @@ import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { QuizScreen } from "./container/QuizScreen/index";
 export default function App() {
   return (
-    <SafeAreaView
-      style={{ flex: 1, flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#ffffff" }}
-    >
+    <SafeAreaView style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#ffffff" }}>
       <QuizScreen />
     </SafeAreaView>
   );
